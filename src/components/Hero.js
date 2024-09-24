ba7c608e0b8e36034988b30ab87c721e0f34c31d
unknown <olawaleprofjobs15@gmail.com>
2024-09-24 03:50:32 -0700

    Hero section fully done

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,7 +1,7 @@
 
 import React from 'react'
 import { FaTwitter, FaLinkedin, FaFacebook, FaArrowDown } from "react-icons/fa"
-import myImage from "../assets/myImage.jpg" 
+import myImage from "../assets/Group.png" 
 
 const Hero = () => {
 
@@ -22,10 +22,59 @@ const Hero = () => {
             icon: <FaLinkedin />
         },
     ];
-  return (
-    <div>
+
+    window.addEventListener("scroll", function () {
+        const downArrow = document.querySelector(".downArrow");
         
-    </div>
+        if (this.scrollY >= 90) downArrow.classList.add("hide-down-arrow")
+        else downArrow.classList.remove("hide-down-arrow")
+    });
+    
+  return (
+    <section className='min-h-screen flex flex-col justify-start items-center p-5 text-center'>
+        <h2 className='text-5xl text-rose-600 font-bold uppercase '>Olawale Fakorede</h2>
+        <h3 className='py-3 text-2xl'>Full Stack Web Development</h3>
+        <p className='max-w-xl font-light text-gray-500'>
+            Hello <span className='animate-pulse text-2xl text-yellow-500'>!!</span>
+            <br />
+            I'm Olawale, a passionate and dedicated  developer with a strong background in web development and design interpretation.
+            <br/>
+            I'm currently looking for opportunities to combine my skills and bring my creativity to life.
+        </p>
+
+        {/* social icons */}
+        <div className='flex justify-evenly py-8 lg:py-16 text-3xl w-full md:w-1/3'>
+            {mySocial.map(({id, link, icon}) => (
+                <a href={link} key={id}
+                target='_blank'
+                rel='noopener noreferrer'
+                className='cursor-pointer duration-300 hover:text-rose-600'
+                >
+                    {icon}
+                </a>
+            ))}
+        </div>
+
+            {/* avatar and resume */}
+            <div>
+                <img 
+                src={myImage} 
+                alt='avatar' 
+                className='w-60 h-60 md:w-72 md:h-72 object-cover object-top bg-gradient-to-b from-rose-600 rounded-xl pt-5'
+                />
+                <a href='/OLAWALE-RESUME.pdf'
+                    download={true}
+                    className='flex items-center justify-center mt-10 bg-gradient-to-tr from-rose-600 to-teal-500 text-white py-2 rounded-lg'
+                >
+                    Resume 
+                </a>
+            </div>
+            
+            {/* arrow down animation */}
+        <div className='mt-10 down-arrow'>
+            <FaArrowDown className='animate-bounce text-2xl text-gray-400' />
+        </div>
+    </section>
   )
 }
 
